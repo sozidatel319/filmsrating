ca3406a8bf8a4acc95a2b9c8b823eb0798c851d0
sozidatel319 <mbogat@yandex.ru>
2020-12-02 15:26:05 +0300

    [ADD] Retrofit

--- app/src/main/java/ru/mikhailskiy/intensiv/network/MovieApiInterface.kt
+++ app/src/main/java/ru/mikhailskiy/intensiv/network/MovieApiInterface.kt
@@ -1,6 +1,32 @@
 package ru.mikhailskiy.intensiv.network
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.mikhailskiy.intensiv.data.MoviesResponse
 
-interface MovieApiInterface {
+
+interface MovieApiInterface{
+
+    @GET("movie/top_rated")
+    fun getTopRatedMovies(@Query("api_key")apiKey: String, @Query("language")language: String):Call<MoviesResponse>
+
+    @GET("movie/now_playing")
+    fun getNowPlayingMovie(@Query("api_key")apiKey:String, @Query("language")language:String): Call<MoviesResponse>
+
+    @GET("movie/upcoming")
+    fun getUpcomingMovies(@Query("api_key")apiKey:String, @Query("language")language:String): Call<MoviesResponse>
+
+    @GET("movie/popular")
+    fun getPopularMovies(@Query("api_key")apiKey:String, @Query("language")language:String): Call<MoviesResponse>
+
+    @GET("movie/get-movie-details")
+    fun getMovieDetails(@Query("api_key")apiKey:String, @Query("language")language:String): Call<MoviesResponse>
+
+    @GET("movie/get-movie-credits")
+    fun getMovieCredits(@Query("api_key")apiKey:String, @Query("language")language:String): Call<MoviesResponse>
+
+    @GET("tv/get-popular-tv-shows")
+    fun getPopularTvShows(@Query("api_key")apiKey:String, @Query("language")language:String): Call<MoviesResponse>
 
 }
--- app/src/main/java/ru/mikhailskiy/intensiv/ui/feed/MovieItem.kt
+++ app/src/main/java/ru/mikhailskiy/intensiv/ui/feed/MovieItem.kt
@@ -12,18 +12,20 @@ class MovieItem(
     private val onClick: (movie: Movie) -> Unit
 ) : Item() {
 
+    private val path: String = "https://image.tmdb.org/t/p/w500/"
+
     override fun getLayout() = R.layout.item_with_text
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.description.text = content.title
-        viewHolder.movie_rating.rating = content.rating
+        //viewHolder.movie_rating.rating = content.
         viewHolder.content.setOnClickListener {
             onClick.invoke(content)
         }
 
         // TODO Получать из модели
         Picasso.get()
-            .load("https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg")
+            .load(path + content.posterPath/*"https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg"*/)
             .into(viewHolder.image_preview)
     }
 }
